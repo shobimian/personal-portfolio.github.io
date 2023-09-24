a8e0aeb72eef9f50f7b08316ec49733cc3b4f95a
Muhammad Shoaib <shoaibshobi620@gmail.com>
2023-09-25 01:27:10 +0500

    initial commit

--- script.js
+++ script.js
@@ -59,3 +59,18 @@ window.onscroll = ()=>{
     menuIcon.classList.remove("bx-x");
     navList.classList.remove("open");
 };
+
+///////active menu///////
+
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
